1105f42f80e69834477cf8f64189387b142f40c3
Wastelander125 <79010377+Wastelander125@users.noreply.github.com>
2022-05-22 17:16:42 +0200

    Malé změny proměnných u State.

--- Assets/Scripts/BookOfSinsState.cs
+++ Assets/Scripts/BookOfSinsState.cs
@@ -7,11 +7,7 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "Ghost", menuName = "Ghost Library", order = 0)]
 public class BookOfSinsState : ScriptableObject {
     
-    public int[] bookPage;
     [SerializeField] [TextArea(5,10)] public string[] ghostText;
-    
-    [SerializeField] Image ghostImage;
-    [SerializeField] string[] ghostAtributes;
     
 
 }
