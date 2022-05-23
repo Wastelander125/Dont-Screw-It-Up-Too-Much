039201bcfc4d22a26073bbffc08dcd90400ba356
Wastelander125 <79010377+Wastelander125@users.noreply.github.com>
2022-05-23 22:45:58 +0200

    Přidány Victory a Loose conditions + Označení hodných a zlých duchů - // 1 - Bad, 2 - Good, 3 - Good, 4 - Bad, 5 - Bad.

--- Assets/Scripts/Ghosts.cs
+++ Assets/Scripts/Ghosts.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Ghosts : MonoBehaviour
 {
     [SerializeField] SpriteRenderer ghostImage;
     [SerializeField] TextMeshProUGUI sinPaper;
-    [SerializeField] GhostPortraits[] portraits;
+    [SerializeField] public GhostPortraits[] portraits;
 
     [SerializeField] SinPaperTextObject[] sinPaperObject;
+    [SerializeField] Button confirmationButton;
 
-    [SerializeField] int ghostNumber = 0;
+    public int ghostNumber = 0;
     [SerializeField] int paperNumber = 0;
+    [SerializeField] int right = 0;
+    [SerializeField] int wrong = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
         sinPaper.text = sinPaperObject[paperNumber].sinText;
-        ghostImage.sprite = portraits[ghostNumber].ghostPortraitImages;
-        
+        ghostImage.sprite = portraits[ghostNumber].ghostPortraitImages;        
     }
 
     // Update is called once per frame
@@ -31,12 +34,13 @@ public class Ghosts : MonoBehaviour
         {
             sinPaper.text = sinPaperObject[paperNumber].sinText;
             ghostImage.sprite = portraits[ghostNumber].ghostPortraitImages;
+            
         }
         else
         {
-            return;
+            confirmationButton.gameObject.SetActive(true);
+            
         }
-        
     }
 
     public void GhostCycling()
@@ -47,4 +51,65 @@ public class Ghosts : MonoBehaviour
         
     }
 
+    public void Decicions()
+    {
+        
+        if(right >= 3)
+        {
+            SceneManager.LoadScene(5);
+        }
+        else
+        {
+            SceneManager.LoadScene(6);
+        }
+    
+    }
+
+    public void Heaven()
+    {
+        if(ghostNumber == 1)
+        {
+            wrong++;
+        }
+        if(ghostNumber == 2)
+        {
+            right++;
+        }
+        if(ghostNumber == 3)
+        {
+            right++;
+        }
+        if(ghostNumber == 4)
+        {
+            wrong++;
+        }
+        if(ghostNumber == 5)
+        {
+            wrong++;
+        }
+        
+    }
+    public void Hell()
+    {
+        if(ghostNumber == 1)
+        {
+            right++;
+        }
+        if(ghostNumber == 2)
+        {
+            wrong++;
+        }
+        if(ghostNumber == 3)
+        {
+            wrong++;
+        }
+        if(ghostNumber == 4)
+        {
+            right++;
+        }
+        if(ghostNumber == 5)
+        {
+            right++;
+        }
+    }
 }
