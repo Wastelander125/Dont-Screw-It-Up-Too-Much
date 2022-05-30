477e7fc0c1d07c10271181eca28f32f695c1d099
Kuba <jakub.zaruba@centrum.cz>
2022-05-30 20:36:08 +0200

    Dokončená hudba

--- Assets/SceneLoadFade/Scripts/FadeScript.cs
+++ Assets/SceneLoadFade/Scripts/FadeScript.cs
@@ -8,11 +8,25 @@ public class FadeScript : MonoBehaviour
 public string scene = "Scene 1 (uvod)";
 public Color loadToColor = Color.green;
 
+private AudioSource _music;
+
 public void GoFade()
 
 {
 
     Initiate.Fade(scene,loadToColor,0.5f);
+
+    if (_music == null)
+        return;
+
+    _music.Play();
+}
+
+private void Awake() 
+
+{
+       
+    _music = GetComponent<AudioSource>();
 }
 
 
